31d468d55cc51f1bdcd3330dbdefcca29a484b10
sabirdy <83651368+sabirdy@users.noreply.github.com>
2021-05-20 15:11:12 +0200

    Add files via upload

---
+++ Paleta.java
@@ -0,0 +1,13 @@
+package Pong;
+
+public class Paleta {
+    public int y;
+    public int alto;
+
+
+    public Paleta(int y, int alto) {
+        this.y = y;
+        this.alto = alto;
+
+    }
+}
