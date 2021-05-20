bb7dbedf67c58b8e37a329508912d4c64d65021a
Zombie-22 <78366541+Zombie-22@users.noreply.github.com>
2021-05-20 15:42:37 +0200

    Add files via upload

---
+++ Godzilla.java
@@ -0,0 +1,13 @@
+package Pong;
+
+public class Godzilla {
+    public int y;
+    public int alto;
+
+
+    public Godzilla(int y, int alto) {
+        this.y = y;
+        this.alto = alto;
+
+    }
+}
